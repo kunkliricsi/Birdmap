1d438bc349bf0b80b1f25d77bb850db1664ff25d
kunkliricsi <kunkli.ricsi@gmail.com>
2020-11-21 10:38:52 +0100

    Modified dashboard

--- Birdmap.API/ClientApp/src/components/dashboard/Dashboard.jsx
+++ Birdmap.API/ClientApp/src/components/dashboard/Dashboard.jsx
@@ -1,31 +1,40 @@
 ﻿import React, { Component } from 'react';
 import { withStyles } from '@material-ui/styles';
 import Services from './services/Services';
+import { blueGrey } from '@material-ui/core/colors';
+import { Box, Grid, IconButton, Paper, Typography } from '@material-ui/core';
 
 const styles = theme => ({
     root: {
         flexGrow: 1,
         padding: '64px',
         backgroundColor: theme.palette.primary.dark,
+    },
+    paper: {
+        backgroundColor: blueGrey[50],
+        height: '60px',
     }
 });
 
 class Dashboard extends Component {
     render() {
-        <Box className={classes.root}>
-            <Grid container spacing={3}>
-                <Grid item xs={8}>
-                    <Services isAdmin={this.props.isAdmin}/>
+        const { classes } = this.props;
+        return (
+            <Box className={classes.root}>
+                <Grid container spacing={3}>
+                    <Grid item xs={8}>
+                        <Services isAdmin={this.props.isAdmin}/>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Paper className={classes.paper} />
+                        <Paper className={classes.paper} />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Paper className={classes.paper} />
+                    </Grid>
                 </Grid>
-                <Grid item xs={4}>
-                    <Paper className={classes.paper} />
-                    <Paper className={classes.paper} />
-                </Grid>
-                <Grid item xs={12}>
-                    <Paper className={classes.paper} />
-                </Grid>
-            </Grid>
-        </Box>
+            </Box>
+        );
     }
 }
 
