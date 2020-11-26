c7e3fcabcf2f2d254bd357ad46b872e95ace12b9
kunkliricsi <kunkli.ricsi@gmail.com>
2020-11-26 12:15:43 +0100

    Added thesis base, and ideas.txt

---
+++ docs/thesis/manual_build.bat
@@ -0,0 +1,10 @@
+set DOCUMENT=thesis
+
+pdflatex --quiet %DOCUMENT%
+bibtex -quiet %DOCUMENT%
+pdflatex --quiet %DOCUMENT%
+pdflatex --quiet %DOCUMENT%
+
+@REM move %DOCUMENT%.pdf ../pdf/%DOCUMENT%.pdf
+
+@del  *.aux *.dvi *.thm *.lof *.log *.lot *.fls *.out *.toc *.bbl *.blg
