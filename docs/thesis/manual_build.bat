c9550ea5b83eaf653c041532ff0c75c61ee847dd
kunkliricsi <kunkli.ricsi@gmail.com>
2020-11-27 16:54:04 +0100

    Modified build batch file, and some formatting

--- docs/thesis/manual_build.bat
+++ docs/thesis/manual_build.bat
@@ -5,6 +5,4 @@ bibtex -quiet %DOCUMENT%
 pdflatex --quiet %DOCUMENT%
 pdflatex --quiet %DOCUMENT%
 
-@REM move %DOCUMENT%.pdf ../pdf/%DOCUMENT%.pdf
-
-@del  *.aux *.dvi *.thm *.lof *.log *.lot *.fls *.out *.toc *.bbl *.blg
+del /s *.aux *.dvi *.thm *.lof *.log *.lot *.fls *.out *.toc *.bbl *.blg
